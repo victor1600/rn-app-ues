1beaaad7ef412dce01f3f8d4ee5d7c8d2f9b84a5
victor <ledzepp1600@gmail.com>
2021-10-07 22:10:08 -0600

    Improved Quiz config

--- app/screens/QuizConfigScreen.js
+++ app/screens/QuizConfigScreen.js
@@ -3,10 +3,26 @@ import { View, StyleSheet } from "react-native";
 import AppButton from "../components/Button";
 import Screen from "./Screen";
 import routes from "../navigation/routes";
+import AppText from "../components/Text";
+import colors from "../config/colors";
 
 function QuizConfigScreen({ route, navigation }) {
   return (
     <Screen>
+      <View style={styles.textContainer}>
+        <AppText style={styles.title}> Realizar cuestionario</AppText>
+        <AppText>
+          Este modulo permite contestar todas las preguntas disponibles para
+          todas las materias.{"\n"}
+          {"\n"}
+          Las preguntas se mostraran en orden aleatorio. Cada pregunta tiene una
+          sola respuesta correcta.
+          {"\n"}
+          {"\n"}La calificacion se muestra cuando se contesta la ultima
+          pregunta.
+        </AppText>
+      </View>
+
       <AppButton
         title="comenzar"
         onPress={() => navigation.navigate(routes.QUIZ)}
@@ -16,7 +32,26 @@ function QuizConfigScreen({ route, navigation }) {
 }
 
 const styles = StyleSheet.create({
+  bulletPointList: {
+    // flex: 1,
+    flex: 1,
+    flexDirection: "row",
+    paddingLeft: 5,
+  },
   container: {},
+  textContainer: {
+    backgroundColor: colors.white,
+    marginTop: 20,
+    marginBottom: 15,
+    padding: 30,
+    alignItems: "center",
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: 28,
+    fontWeight: "500",
+    marginBottom: 15,
+  },
 });
 
 export default QuizConfigScreen;
