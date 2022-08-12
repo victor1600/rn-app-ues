60988474360965a5abe1e181a55447a15a04d7a9
geovannyPaiz <geovanny_paiz@hotmail.com>
2022-08-12 16:34:15 -0600

    fixing supplementary material

--- app/screens/SupplementaryMaterialScreen.js
+++ app/screens/SupplementaryMaterialScreen.js
@@ -13,59 +13,59 @@ import Screen from "./Screen";
 import materials from "../api/materials";
 
 function SupplementaryMaterialScreen({ route }) {
-  const topic = route.params;
-  const getMaterialsApi = useApi(materials.getMaterials);
-  const refresh = useRefresh(getMaterialsApi, topic.id);
+	const { topic } = route.params;
+	const getMaterialsApi = useApi(materials.getMaterials);
+	const refresh = useRefresh(getMaterialsApi, topic.id);
 
-  useEffect(() => {
-    getMaterialsApi.request(topic.id);
-  }, []);
+	useEffect(() => {
+		getMaterialsApi.request(topic.id);
+	}, []);
 
-  return (
-    <Screen>
-      <ActivityIndicator visible={getMaterialsApi.loading} />
-      {getMaterialsApi.error && (
-        <>
-          <AppText>Couldn't retrieve Materials</AppText>
-          <Button
-            title="Retry"
-            onPress={() => getMaterialsApi.request(topic.id)}
-          />
-        </>
-      )}
-      <View style={styles.text_container}>
-        <AppText style={styles.title}>{topic.name}</AppText>
-      </View>
-      <FlatList
-        data={getMaterialsApi.data}
-        keyExtractor={(material) => material.id.toString()}
-        onRefresh={refresh.onRefresh}
-        refreshing={refresh.refreshing}
-        renderItem={({ item }) => (
-          <ListItem
-            icon="open-in-new"
-            text={item.texto}
-            onPress={() => Linking.openURL(item.archivo)}
-          />
-        )}
-        ListEmptyComponent={() => (
-          <ContentNotFound title="Material de estudio" />
-        )}
-      />
-    </Screen>
-  );
+	return (
+		<Screen>
+			<ActivityIndicator visible={getMaterialsApi.loading} />
+			{getMaterialsApi.error && (
+				<>
+					<AppText>Couldn't retrieve Materials</AppText>
+					<Button
+						title="Retry"
+						onPress={() => getMaterialsApi.request(topic.id)}
+					/>
+				</>
+			)}
+			<View style={styles.text_container}>
+				<AppText style={styles.title}>{topic.name}</AppText>
+			</View>
+			<FlatList
+				data={getMaterialsApi.data}
+				keyExtractor={(material) => material.id.toString()}
+				onRefresh={refresh.onRefresh}
+				refreshing={refresh.refreshing}
+				renderItem={({ item }) => (
+					<ListItem
+						icon="open-in-new"
+						text={item.texto}
+						onPress={() => Linking.openURL(item.archivo)}
+					/>
+				)}
+				ListEmptyComponent={() => (
+					<ContentNotFound title="Material de estudio" />
+				)}
+			/>
+		</Screen>
+	);
 }
 
 const styles = StyleSheet.create({
-  text_container: {
-    marginTop: 10,
-    alignItems: "center",
-  },
-  title: {
-    fontWeight: "800",
-    fontSize: 20,
-    marginBottom: 20,
-  },
+	text_container: {
+		marginTop: 10,
+		alignItems: "center",
+	},
+	title: {
+		fontWeight: "800",
+		fontSize: 20,
+		marginBottom: 20,
+	},
 });
 
 export default SupplementaryMaterialScreen;
